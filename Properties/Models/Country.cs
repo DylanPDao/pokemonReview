67efa5341919acbc97ebcc490653777876730092
Dylan Dao <dylanpdao@gmail.com>
2024-02-06 22:06:07 -0800

    created models

---
+++ Properties/Models/Country.cs
@@ -0,0 +1,11 @@
+using System.Security.AccessControl;
+
+namespace PokemonReviewApp.Models
+
+{
+  public class Country
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
+}
---
+++ Properties/Models/Owner.cs
@@ -0,0 +1,12 @@
+using System.Security.AccessControl;
+
+namespace PokemonReviewApp.Models
+
+{
+  public class Owner
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Gym { get; set; }
+  }
+}
