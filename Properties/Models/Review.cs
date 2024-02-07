11072d86f16d42dde8c0a52ce7d30bd15e43a65f
Dylan Dao <dylanpdao@gmail.com>
2024-02-06 22:06:41 -0800

    more models created

---
+++ Properties/Models/Review.cs
@@ -0,0 +1,12 @@
+using System.Security.AccessControl;
+
+namespace PokemonReviewApp.Models
+
+{
+  public class Review
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+  }
+}
---
+++ Properties/Models/Reviewer.cs
@@ -0,0 +1,12 @@
+using System.Security.AccessControl;
+
+namespace PokemonReviewApp.Models
+
+{
+  public class Reviewer
+  {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+  }
+}
